18971fff4d631554269ca2a6c8c333b81d7e5daf
lisaabolo <lisaabolo@gmail.com>
2026-08-10 18:07:01 +0100

    First steps in Selenium Automation with C#
    Simple automation testing with Google Chrome browser.

---
+++ Selenium Automation/Program.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium_Automation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a reference for Chrome Driver
+            IWebDriver driver = new ChromeDriver();
+
+            // Go to Google page
+            driver.Navigate().GoToUrl("https://www.google.com/");
+
+            // Make the browser full screen
+            driver.Manage().Window.Maximize();
+
+            // Find the element
+            IWebElement webElement = driver.FindElement(By.Name("q"));
+
+            // Type something in the search box
+            webElement.SendKeys("This is the best automation course EVER!");
+
+            // Close the browser
+            driver.Quit();
+        }
+    }
+}
---
+++ Selenium Automation/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Selenium Automation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Selenium Automation")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7f9213c7-38bf-478d-9529-eb2db5895dcb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
